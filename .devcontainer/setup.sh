b16ad1d2e13ade456ac7e0fff4770659936d8192
zdu-idm <zdu@idmod.org>
2025-04-26 10:43:53 -0700

    make devcontainer for hiv

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,16 +7,11 @@ sudo apt install -y build-essential curl git unzip zip
 
 # Install additional Python packages
 pip install --upgrade pip
-pip install numpy pandas matplotlib seaborn jupyterlab ipykernel jupytext
+pip install numpy pandas matplotlib seaborn jupyterlab ipykernel jupytext ipywidgets
 
 
-# Verify Docker access
-# sudo usermod -aG docker $USER
-# sudo chown root:docker /var/run/docker.sock
-# sudo chmod 666 /var/run/docker.sock
-# docker --version
-# sudo chown -R $USER:$USER /workspaces
-
+# dev install emodpy-malaria (can be replaced later by pip install emodpy-malaria from jfrog)
+pip install -e . --index-url=https://packages.idmod.org/api/pypi/pypi-production/simple
 # install idmtools
-pip install idmtools[full] --index-url=https://packages.idmod.org/api/pypi/pypi-production/simple
-pip install idmtools-test --index-url=https://packages.idmod.org/api/pypi/pypi-production/simple
+pip install idmtools[full] --index-url=https://packages.idmod.org/api/pypi/pypi-production/simple --upgrade --force-reinstall
+pip install idmtools-test --index-url=https://packages.idmod.org/api/pypi/pypi-production/simple  --upgrade --force-reinstall
